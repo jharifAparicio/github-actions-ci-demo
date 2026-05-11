641c4ab91b88d8dcac7582045c6a21227228ac34
Jharif Nelson Aparicio Casillas <jharif.aparicio.casillas@usfx.bo>
2026-05-11 17:02:58 -0400

    test: fuerza un error de linting para probar el pipeline

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.get("/users", (req, res) => {
   ]);
 });
 
-const unusedVariable = "esto causara un error de linting";
+const unusedVariable = "esto causara un error de linting
 
 module.exports = app;
